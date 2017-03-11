691d8e552792bb0cfb7b3e3d1d79d8fcb943bed1
Tien Duong, Zen <tienduong.nk@gmail.com>
2017-03-11 11:09:35 +0800

    Add Messenger
    Implement Slack

--- src/posts/core/messenger.py
+++ src/posts/core/messenger.py
@@ -1,7 +1,12 @@
+from . import slack
+
+
 def send(reciver_id, content_string):
-    print("______ SENDING TEXT - START _________")
-    print("TO:")
-    print(reciver_id)
-    print("MESSAGE:")
-    print(content_string)
-    print("______ SENDING TEXT - END _________")
+    msg = "______ SENDING TEXT - START _________\n"
+    msg += "\t To:"
+    msg += "\t "+reciver_id + "\n"
+    msg += "\t Message:\n"
+    msg += "\t" + content_string + "\n"
+    msg += "______ SENDING TEXT - END _________"
+    print(msg)
+    slack.send_msg(msg)
